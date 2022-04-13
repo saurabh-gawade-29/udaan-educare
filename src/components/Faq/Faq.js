44ce52d4330421654f22562d076e56a91e678b42
Saurabh Gawade <saurabhgawade2920@gmail.com>
2022-04-13 23:01:22 +0530

    Faq and terms page set

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,11 +1,194 @@
 import React, { Component } from "react";
 import "./Faq.scss";
+import FAQ from "../../assets/Faq.jpg";
 
 export class Faq extends Component {
   render() {
     return (
       <div>
-        <h1>FAQ</h1>
+        <div className="Faq-Wrapper">
+          <div className="container">
+            <div className="Faq-heading text-center my-4">
+              <h1>
+                Frequently Asked <span className="text-orange">Questions</span>
+              </h1>
+            </div>
+            <div className="faq-container my-4">
+              <div className="row">
+                {/* Col6 */}
+                <div className="col-12 col-sm-12 col-md-6 col-lg-6 my-4">
+                  <div className="faq-img-container ">
+                    <div className="card p-1 shadow">
+                      <img src={FAQ} alt="" className="img-fluid" />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-12 col-sm-12 col-md-6 col-lg-6 my-4">
+                  <div
+                    className="accordion accordion-flush"
+                    id="accordionFlushExample"
+                  >
+                    {/* Question Number 1 */}
+                    <div className="accordion-item">
+                      <h2 className="accordion-header" id="flush-headingOne">
+                        <button
+                          className="accordion-button collapsed"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#flush-collapseOne"
+                          aria-expanded="false"
+                          aria-controls="flush-collapseOne"
+                        >
+                          How Online Lectures are Scheduled?
+                        </button>
+                      </h2>
+                      <div
+                        id="flush-collapseOne"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="flush-headingOne"
+                        data-bs-parent="#accordionFlushExample"
+                      >
+                        <div className="accordion-body">
+                          You Will be informed 1 week ago with a PDF of 1month
+                          time-table of subject and the teachers who will take
+                          the lectures and other information
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Question Number 2 */}
+                    <div className="accordion-item">
+                      <h2 className="accordion-header" id="flush-headingTwo">
+                        <button
+                          className="accordion-button collapsed"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#flush-collapseTwo"
+                          aria-expanded="false"
+                          aria-controls="flush-collapseTwo"
+                        >
+                          How Can I Ask Doubt In Online Lecture?
+                        </button>
+                      </h2>
+                      <div
+                        id="flush-collapseTwo"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="flush-headingTwo"
+                        data-bs-parent="#accordionFlushExample"
+                      >
+                        <div className="accordion-body">
+                          All the online lectures are not more then 10 students
+                          only and if you are taking Private/Home tution then
+                          one to one nobody else, now for doubt since batches
+                          are small it will be very interactive and you can
+                          raise your hand and ask doubt easily also teachers can
+                          ask you questions as well so better be attentive in
+                          class.
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Question Number 3 */}
+                    <div className="accordion-item">
+                      <h2 className="accordion-header" id="flush-headingThree">
+                        <button
+                          className="accordion-button collapsed"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#flush-collapseThree"
+                          aria-expanded="false"
+                          aria-controls="flush-collapseThree"
+                        >
+                          I want private tuition but the location is not in
+                          Mumbai/Thane/Navi Mumbai?
+                        </button>
+                      </h2>
+                      <div
+                        id="flush-collapseThree"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="flush-headingThree"
+                        data-bs-parent="#accordionFlushExample"
+                      >
+                        <div className="accordion-body">
+                          Well we have good news for you as after great success
+                          in physical location we started online private tution
+                          as well so whether you are in J&K, Tamil Nadu, Gujrat
+                          to North East, every location is covered by Excellence
+                          Educare.
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Question Number 4 */}
+                    <div className="accordion-item">
+                      <h2 className="accordion-header" id="flush-headingFour">
+                        <button
+                          className="accordion-button collapsed"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#flush-collapseFour"
+                          aria-expanded="false"
+                          aria-controls="flush-collapseFour"
+                        >
+                          I Am Weak In Particular Subject Can I Take Single
+                          Subject For My JEE/NEET Preparation?
+                        </button>
+                      </h2>
+                      <div
+                        id="flush-collapseFour"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="flush-headingFour"
+                        data-bs-parent="#accordionFlushExample"
+                      >
+                        <div className="accordion-body">
+                          Well guess what, we got you covered as well, i would
+                          like to suggest you that if you are weak in particular
+                          subject you should definitely go for single subject
+                          private tution Home/Online so we will take care of
+                          your subject and we will make the subject as a strong
+                          subject for you.
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Question Number 5 */}
+                    <div className="accordion-item">
+                      <h2 className="accordion-header" id="flush-headingFive">
+                        <button
+                          className="accordion-button collapsed"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#flush-collapseFive"
+                          aria-expanded="false"
+                          aria-controls="flush-collapseFive"
+                        >
+                          My Parents Can’t Pay This Much Fee At Once, Can You
+                          Help With That?
+                        </button>
+                      </h2>
+                      <div
+                        id="flush-collapseFive"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="flush-headingFive"
+                        data-bs-parent="#accordionFlushExample"
+                      >
+                        <div className="accordion-body">
+                          We Excellence Educare as responsible classes
+                          understand that as we charge lowest then any other
+                          coaching in the country still we understand the
+                          current situation and how badly people are affected so
+                          we can arrange a meeting for that and can talk about
+                          EMI facility as well which Excellence Educare provide.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/Terms/Terms.js
+++ src/components/Terms/Terms.js
@@ -5,7 +5,90 @@ export class Terms extends Component {
   render() {
     return (
       <div>
-        <h1>Terms & Conditions</h1>
+        <div className="container my-3">
+          <h1>
+            Terms & <span className="text-orange">Conditions</span>
+          </h1>
+          <div className="bg-light rounded-3 my-3 shadow-sm">
+            <div className="terms-main-container p-4">
+              <h1 className="display-6 fw-bold">Overview</h1>
+              <p className="col-md-12 fs-6">
+                Using a series of utilities, you can create this jumbotron, just
+                like the one in previous versions of Bootstrap. Check out the
+                examples below for how you can remix and restyle it to your
+                liking. Lorem ipsum, dolor sit amet consectetur adipisicing
+                elit. Dolores, animi. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Distinctio, unde ipsam. Neque vitae dolor quae
+                perferendis, consequuntur provident soluta sed deleniti omnis
+                animi iusto dolorum ducimus dolorem reiciendis mollitia
+                recusandae itaque sint laudantium quisquam eligendi ipsam? Omnis
+                blanditiis, veritatis praesentium doloribus consequuntur, qui
+                possimus aperiam iste dignissimos quam dicta nam. Lorem ipsum,
+                dolor sit amet consectetur adipisicing elit. Enim, aspernatur
+                eum odio sunt voluptatibus libero qui omnis exercitationem.
+                Impedit incidunt quibusdam delectus doloremque quae aperiam et
+                eligendi quaerat natus cupiditate.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="container my-3">
+          <div className="row align-items-md-stretch">
+            <div className="col-md-6 my-2">
+              <div className="h-100 p-4 bg-white border rounded-3 shadow-sm">
+                <h4>SECTION 1 – ONLINE STORE TERMS</h4>
+                <p>
+                  By agreeing to these Terms of Service, you represent that you
+                  are at least the age of majority in your state or province of
+                  residence, or that you are the age of majority in your state
+                  or province of residence and you have given us your consent to
+                  allow any of your minor dependents to use this site.
+                </p>
+              </div>
+            </div>
+
+            <div className="col-md-6 my-2">
+              <div className="h-100 p-4 bg-white border rounded-3 shadow-sm">
+                <h4>SECTION 2 – GENERAL CONDITIONS</h4>
+                <p>
+                  You understand that your content (not including credit card
+                  information), may be transferred unencrypted and involve (a)
+                  transmissions over various networks; and (b) changes to
+                  conform and adapt to technical requirements of connecting
+                  networks or devices. Credit card information is always
+                  encrypted during transfer over networks.
+                </p>
+              </div>
+            </div>
+
+            <div className="col-md-6 my-2">
+              <div className="h-100 p-4 bg-white border rounded-3 shadow-sm">
+                <h4>SECTION 2 – GENERAL CONDITIONS</h4>
+                <p>
+                  You understand that your content (not including credit card
+                  information), may be transferred unencrypted and involve (a)
+                  transmissions over various networks; and (b) changes to
+                  conform and adapt to technical requirements of connecting
+                  networks or devices. Credit card information is always
+                  encrypted during transfer over networks.
+                </p>
+              </div>
+            </div>
+
+            <div className="col-md-6 my-2">
+              <div className="h-100 p-4 bg-white border rounded-3 shadow-sm">
+                <h4>SECTION 1 – ONLINE STORE TERMS</h4>
+                <p>
+                  By agreeing to these Terms of Service, you represent that you
+                  are at least the age of majority in your state or province of
+                  residence, or that you are the age of majority in your state
+                  or province of residence and you have given us your consent to
+                  allow any of your minor dependents to use this site.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
