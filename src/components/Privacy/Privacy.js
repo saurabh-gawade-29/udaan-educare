8ee871190fcac78c92a0a724e0a9b8fd48774576
Saurabh Gawade <saurabhgawade2920@gmail.com>
2022-04-14 20:31:06 +0530

    Privacy And Refund UI Added

--- src/components/Privacy/Privacy.js
+++ src/components/Privacy/Privacy.js
@@ -5,7 +5,91 @@ export class Privacy extends Component {
   render() {
     return (
       <div>
-        <h1>Privacy</h1>
+        <div className="container my-4">
+          <div className="heading">
+            <h1>
+              Privacy <span className="text-orange">Policy</span>
+            </h1>
+          </div>
+          <div className="privacy-second-container my-4">
+            <div className="card bg-light border border-4 rounded-3 p-4 border-top-0 border-end-0 border-bottom-0 shadow">
+              <h5 className="display-6">Information Collected</h5>
+              <p className="text-muted">
+                Our Site collects information regarding domain names, IP address
+                (static or dynamic IP-address that sometimes point to a
+                particular identifiable computer or device), type of browser,
+                operating system, time of access and referring/exit web site
+                addresses. This information is used by PrepBootstrap. for
+                operational purposes, to maintain service quality and to track
+                site trends and statistics. Most of the time, this information
+                does not personally identify you. We also collect any personal
+                information that you choose to provide when you use this Site.
+                This may include, but is not limited to the events when you:
+                create an account, order and/or download products, contact our
+                sales department, post content in our forums or interact with
+                other areas of our support center. This information of personal
+                nature may include, your name, e-mail address, and any other
+                information selected by you to share. We may collect any
+                additional personal information you knowingly and voluntarily
+                provide through such posting, contact or interaction, in cases
+                when you post content in the forums, interact with other areas
+                of our support center, or contact our sales department or.
+              </p>
+            </div>
+          </div>
+          <div className="privacy-second-container my-4">
+            <div className="card bg-light border border-4 rounded-3 p-4 border-top-0 border-end-0 border-bottom-0 shadow">
+              <h5 className="display-6">Information Collected</h5>
+              <p className="text-muted">
+                Our Site collects information regarding domain names, IP address
+                (static or dynamic IP-address that sometimes point to a
+                particular identifiable computer or device), type of browser,
+                operating system, time of access and referring/exit web site
+                addresses. This information is used by PrepBootstrap. for
+                operational purposes, to maintain service quality and to track
+                site trends and statistics. Most of the time, this information
+                does not personally identify you. We also collect any personal
+                information that you choose to provide when you use this Site.
+                This may include, but is not limited to the events when you:
+                create an account, order and/or download products, contact our
+                sales department, post content in our forums or interact with
+                other areas of our support center. This information of personal
+                nature may include, your name, e-mail address, and any other
+                information selected by you to share. We may collect any
+                additional personal information you knowingly and voluntarily
+                provide through such posting, contact or interaction, in cases
+                when you post content in the forums, interact with other areas
+                of our support center, or contact our sales department or.
+              </p>
+            </div>
+          </div>
+          <div className="privacy-second-container my-4">
+            <div className="card bg-light border border-4 rounded-3 p-4 border-top-0 border-end-0 border-bottom-0 shadow">
+              <h5 className="display-6">Information Collected</h5>
+              <p className="text-muted">
+                Our Site collects information regarding domain names, IP address
+                (static or dynamic IP-address that sometimes point to a
+                particular identifiable computer or device), type of browser,
+                operating system, time of access and referring/exit web site
+                addresses. This information is used by PrepBootstrap. for
+                operational purposes, to maintain service quality and to track
+                site trends and statistics. Most of the time, this information
+                does not personally identify you. We also collect any personal
+                information that you choose to provide when you use this Site.
+                This may include, but is not limited to the events when you:
+                create an account, order and/or download products, contact our
+                sales department, post content in our forums or interact with
+                other areas of our support center. This information of personal
+                nature may include, your name, e-mail address, and any other
+                information selected by you to share. We may collect any
+                additional personal information you knowingly and voluntarily
+                provide through such posting, contact or interaction, in cases
+                when you post content in the forums, interact with other areas
+                of our support center, or contact our sales department or.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
